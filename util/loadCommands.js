d01c3f5ebdd87f57e3246b63ce455a17f0b93dab
PoorLaszlo <79516211+PoorLaszlo@users.noreply.github.com>
2022-03-09 15:57:23 +0100

    Update loadCommands.js

--- util/loadCommands.js
+++ util/loadCommands.js
@@ -1,5 +1,8 @@
 const path = require("path");
 const fs = require("fs");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const getConfig = require("../util/getConfig");
 
 const LoadCommands = () => {
   return new Promise(async (resolve) => {
@@ -35,9 +38,25 @@ const LoadDirectory = (dir) => {
         if (dir == "context") commands.push(cmd.command);
         else commands.push(cmd);
         if (r) resolve(commands);
+        async () => {
+          const config = await getConfig();
+          const rest = new REST({ version: "9" }).setToken(config.token);
+          const commands = await LoadCommands().then((cmds) => {
+            return [].concat(cmds.slash).concat(cmds.context);
+          });
+        
+          console.log("Deploying commands to global...");
+          await rest
+            .put(Routes.applicationCommands(config.clientId), {
+              body: commands,
+            })
+            .catch(console.log);
+          console.log("Successfully deployed commands!");
+        };
       });
     });
   });
 };
 
 module.exports = LoadCommands;
+
